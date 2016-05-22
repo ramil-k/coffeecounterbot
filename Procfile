044c9bee0420e977e1ff5fbd45763cb1ffaaccef
Ramil Karimov <ramil@Ramils-iMac.local>
2016-05-21 19:27:58 -0700

    heroku bot first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/coffeecounterbot-standalone.jar clojure.main -m coffeecounterbot.web
---
+++ test/coffeecounterbot/web_test.clj
@@ -0,0 +1,6 @@
+(ns coffeecounterbot.web-test
+  (:require [clojure.test :refer :all]
+            [coffeecounterbot.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))

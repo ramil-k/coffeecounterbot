bf4c4f29d99893eee631193221f520dc2e015de9
Ramil Karimov <ramil@Ramils-iMac.local>
2016-05-22 00:46:43 -0700

    dumb bot upd

--- src/coffeecounterbot/web.clj
+++ src/coffeecounterbot/web.clj
@@ -1,15 +1,18 @@
 (ns coffeecounterbot.web
   (:require [telegram.client]
-            [environ.core :refer [env]])
-  )
+            [environ.core :refer [env]]))
+
+(defn process-upd [upd]
+  (let [token (env :token)
+        chat-id (-> upd :message :chat :id)]
+    (telegram.client/get-updates token (inc (upd :update_id)))
+    (telegram.client/send-message token chat-id ["SPACE"])))
 
 (defn -main []
   (let [token (env :token)]
     (while true
       (let [updates ((telegram.client/get-updates token) :result)]
         (dorun
-          (map #((let [upd % chat-id (-> upd :message :chat :id)]
-                   (telegram.client/get-updates token (inc (upd :update_id)))
-                   (telegram.client/send-message token chat-id ["SPACE"])))
-               updates))))
-    (Thread/sleep 300)))
+          (map #(process-upd %)
+               updates)))
+      (Thread/sleep 3000))))
